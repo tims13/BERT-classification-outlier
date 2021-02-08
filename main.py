5deeb0b4ca204adee1078cd7c5545761305ec22d
tim <405362140@qq.com>
2021-02-09 00:04:47 +0800

    modified main.py

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ from train import train
 import matplotlib.pyplot as plt
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import seaborn as sns
+from utils import load_metrics
 
 des_folder = 'record'
+num_epochs = 10
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(device)
@@ -19,7 +21,7 @@ optimizer = optim.Adam(model.parameters(), lr=2e-5)
 train_iter, valid_iter, test_iter = load_data(device=device)
 
 print("start training...")
-train(model=model, optimizer=optimizer, train_loader=train_iter, valid_loader=valid_iter, num_epochs=1, eval_every=len(train_iter) // 2, file_path=des_folder, device=device, best_valid_loss=float("Inf"))
+train(model=model, optimizer=optimizer, train_loader=train_iter, valid_loader=valid_iter, num_epochs=num_epochs, eval_every=len(train_iter) // 2, file_path=des_folder, device=device, best_valid_loss=float("Inf"))
 
 # save the training iteration figure
 train_loss_list, valid_loss_list, global_steps_list = load_metrics(device, des_folder + '/metrics.pt')
