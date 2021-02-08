53e37877793300925ab53d779a5869ddc3ad7e1d
tims13 <405362140@qq.com>
2021-02-09 01:12:30 +0800

    fix bug

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import seaborn as sns
 from utils import load_metrics, load_checkpoint
 from evaluate import evaluate
 
-des_folder = 'record'
+des_folder = 'record/'
 num_epochs = 10
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
@@ -25,7 +25,7 @@ print("start training...")
 # train(model=model, optimizer=optimizer, train_loader=train_iter, valid_loader=valid_iter, num_epochs=num_epochs, eval_every=len(train_iter) // 2, file_path=des_folder, device=device, best_valid_loss=float("Inf"))
 
 # save the training iteration figure
-train_loss_list, valid_loss_list, global_steps_list = load_metrics(device, des_folder + '/metrics.pt')
+train_loss_list, valid_loss_list, global_steps_list = load_metrics(des_folder + '/metrics.pt', device)
 plt.plot(global_steps_list, train_loss_list, label='Train')
 plt.plot(global_steps_list, valid_loss_list, label='Valid')
 plt.xlabel('Global Steps')
@@ -35,5 +35,5 @@ plt.savefig(des_folder + 'train_iter.png')
 
 # evaluate
 best_model = BERT().to(device)
-load_checkpoint(des_folder + '/model.pt', best_model)
+load_checkpoint(des_folder + '/model.pt', best_model, device)
 evaluate(best_model, test_iter, device, des_folder)
