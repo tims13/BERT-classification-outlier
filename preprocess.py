e84b6c00b982157fb50533bc0eec889827eb91d4
tims13 <405362140@qq.com>
2021-04-07 20:14:44 +0800

    informative

--- preprocess.py
+++ preprocess.py
@@ -7,12 +7,12 @@ from utils import trim_string, sentence_token_nltk
 
 des_path = 'sentence/'
 data_path = des_path + 'annotation_sentence.xlsx'
-data_need_path = des_path + 'needs_com.csv'
+
 data_pos_csv_path = des_path + 'data_pos.csv'
 data_neg_csv_path = des_path + 'data_neg.csv'
 data_irre_csv_path = des_path + 'data_irre.csv'
 data_novel_csv_path = des_path + 'data_novel.csv'
-data_need_csv_path = des_path + 'data_need_com.csv'
+
 
 train_test_ratio = 0.90
 train_valid_ratio = 0.80
@@ -40,57 +40,47 @@ for i in range(4):
     data_t.reset_index(drop=True, inplace=True)
     data_t.to_csv(data_paths[i], header=headers[i], index=0)
 
-# need
-'''
-data_need = pd.read_csv(data_need_path)
-sentences = []
-for i in range(data_need.shape[0]):
-    s = data_need.iloc[i][0]
-    sentence = sentence_token_nltk(s)
-    sentences.append(sentence[0])
-df_need_sentence = pd.DataFrame(sentences, columns=['text'])
-df_need_sentence.to_csv(data_need_csv_path)
-'''
 data_pos = pd.read_csv(data_pos_csv_path)
-data_pos['label'] = 0
+data_pos['label'] = 1
 data_pos.rename(columns={'pos': 'text'}, inplace=True)
 data_pos['text'] = data_pos['text'].apply(trim_string)
 
 data_neg = pd.read_csv(data_neg_csv_path)
-data_neg['label'] = 0
+data_neg['label'] = 1
 data_neg.rename(columns={'neg': 'text'}, inplace=True)
 data_neg['text'] = data_neg['text'].apply(trim_string)
 
-# preprocess unknow intent / novelty
+data_irre = pd.read_csv(data_irre_csv_path)
+data_irre['label'] = 0
+data_irre.rename(columns={'irre': 'text'}, inplace=True)
+data_irre['text'] = data_irre['text'].apply(trim_string)
+
 data_novel = pd.read_csv(data_novel_csv_path)
 data_novel['label'] = 1
 data_novel.rename(columns={'novel': 'text'}, inplace=True)
 data_novel['text'] = data_novel['text'].apply(trim_string)
 
-# need
-data_need = pd.read_csv(data_need_csv_path)
-data_need['label'] = 1
-data_need['text'] = data_need['text'].apply(trim_string)
-
 # Train - Test
 df_pos_full_train, df_pos_test = train_test_split(data_pos, train_size = train_test_ratio, random_state=1)
 df_neg_full_train, df_neg_test = train_test_split(data_neg, train_size = train_test_ratio, random_state=1)
-df_need_full_train, df_need_test = train_test_split(data_need, train_size = train_test_ratio, random_state=1)
+df_irre_full_train, df_irre_test = train_test_split(data_irre, train_size = train_test_ratio, random_state=1)
+df_novel_full_train, df_novel_test = train_test_split(data_novel, train_size = train_test_ratio, random_state=1)
 
 # Train - valid
 df_pos_train, df_pos_valid = train_test_split(df_pos_full_train, train_size = train_valid_ratio, random_state=1)
 df_neg_train, df_neg_valid = train_test_split(df_neg_full_train, train_size = train_valid_ratio, random_state=1)
-df_need_train, df_need_valid = train_test_split(df_need_full_train, train_size = train_valid_ratio, random_state=1)
+df_irre_train, df_irre_valid = train_test_split(df_irre_full_train, train_size = train_valid_ratio, random_state=1)
+df_novel_train, df_novel_valid = train_test_split(df_novel_full_train, train_size = train_valid_ratio, random_state=1)
 
-df_train = pd.concat([df_pos_train, df_neg_train, df_need_train], ignore_index=True, sort=False)
-df_valid = pd.concat([df_pos_valid, df_neg_valid, df_need_valid], ignore_index=True, sort=False)
-df_test = pd.concat([df_pos_test, df_neg_test, df_need_test], ignore_index=True, sort=False)
+df_train = pd.concat([df_pos_train, df_neg_train, df_irre_train, df_novel_train], ignore_index=True, sort=False)
+df_valid = pd.concat([df_pos_valid, df_neg_valid, df_irre_valid, df_novel_valid], ignore_index=True, sort=False)
+df_test = pd.concat([df_pos_test, df_neg_test, df_irre_test, df_novel_test], ignore_index=True, sort=False)
 
+print('irre:', str(data_irre.shape))
 print('train-valid-test', str(df_train.shape), str(df_valid.shape), str(df_test.shape))
 
 df_train.to_csv(des_path + 'train.csv', index=False)
 df_valid.to_csv(des_path + 'valid.csv', index=False)
 df_test.to_csv(des_path + 'test.csv', index=False)
-data_novel.to_csv(des_path + 'novel.csv', index=False)
 
 print("Preprocess finished")
